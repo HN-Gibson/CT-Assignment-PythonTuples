b3535ec1021cd88f186dcd69ae75e26108f68491
H.N. Gibson <revhngibson@gmail.com>
2024-09-10 10:20:45 -0500

    completed the assignment

--- tuple_mastery_in_python.py
+++ tuple_mastery_in_python.py
@@ -7,16 +7,11 @@
 
 #created the function to be called and print formatted flight itineraries
 def flight_itinerary(list):
-    #set a default value for the itinerary number
-    i=1
     #created a list to be combined later into a formatted string
     itinerary=[]
-    #called a loop for each tuple in list and unpacked into variables
-    for travelers_name,origin,destination in list:
-        #formated and appended the variables into the list as a string
-        itinerary.append(f"Itinerary {i}: {travelers_name} - From {origin} to {destination}")
-        #increased the value of the itinerary number by 1
-        i+=1
+    for i, (travelers_name,origin,destination) in enumerate(list):
+    #formated and appended the variables into the list as a string
+        itinerary.append(f"Itinerary {i + 1}: {travelers_name} - From {origin} to {destination}")
     #returned the list joined and formatted as directed    
     return "\n".join(itinerary)
         
